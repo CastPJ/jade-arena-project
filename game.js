bbfff306b5f54f6b2172af90f75b8c6425994378
CastPJ <JPajak.dev@gmail.com>
2024-08-27 11:51:09 +0200

    Info layout for champions

---
+++ game.js
@@ -0,0 +1,83 @@
+const infoBar = document.getElementById("info-bar");
+infoBar.innerHTML = `
+         <div class="row">
+            <div class="col-3">
+              <div class="row info-img">
+                <img
+                  class="p-0"
+                  src="/images/Champions/Kenshin/Kenshin-skill-1.jpeg"
+                  alt=""
+                />
+              </div>
+              <div class="row info-add">
+                <div class="col info-energy">1x[--] 1x[--]</div>
+                <div class="col info-cooldown">Cd: 1</div>
+              </div>
+            </div>
+            <div class="col-9 skill-description">
+              <div class="row skill-name">Lightning thrust</div>
+              <div class="row skill-short-description">
+                DMG | 25 | single target
+              </div>
+              <div class="row skill-long-description">
+                Kenshin dashes with his katana, infused with the spark of
+                lihtning, dealing 25 DMG to single target.
+              </div>
+            </div>
+          </div>`;
+// infoBar.innerHTML = `<div class="row info-image-row">
+//             <div class="col image-col">
+//               <img
+//                 class="img-avatar info-img-avatar"
+//                 src="/images/Champions/Silvia/Silvia-avatar.jpeg"
+//                 alt=""
+//               />
+//             </div>
+//             <div class="col image-col">
+//               <img
+//                 class="img-skill info-img-avatar"
+//                 src="/images/Champions/Silvia/Silvia-skill-1.jpeg"
+//                 alt=""
+//               />
+//             </div>
+//             <div class="col image-col">
+//               <img
+//                 class="img-skill"
+//                 src="/images/Champions/Silvia/Silvia-skill-2.jpeg"
+//                 alt=""
+//               />
+//             </div>
+//             <div class="col image-col">
+//               <img
+//                 class="img-skill"
+//                 src="/images/Champions/Silvia/Silvia-skill-3.jpeg"
+//                 alt=""
+//               />
+//             </div>
+//             <div class="col image-col">
+//               <img
+//                 class="img-skill"
+//                 src="/images/Champions/Silvia/Silvia-skill-4.jpeg"
+//                 alt=""
+//               />
+//             </div>
+//           </div>
+//           <div class="row info-name-row">
+//             <div class="col info-name-col">Silvia</div>
+//             <div class="col info-skill-name-col">
+//               <h6 class="m-0">Thrust</h6>
+//               DMG | 25 | single target
+//             </div>
+//             <div class="col info-skill-name-col">
+//               <h6 class="m-0">Thrust</h6>
+//               DMG | 25 | single target
+//             </div>
+//             <div class="col info-skill-name-col">
+//               <h6 class="m-0">Thrust</h6>
+//               DMG | 25 | single target
+//             </div>
+//             <div class="col info-skill-name-col">
+//               <h6 class="m-0">Thrust</h6>
+//               DMG | 25 | single target
+//             </div>
+//           </div>`;
